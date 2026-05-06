5c09239e01f5c66c7254cae3e77c198c647a2fb4
Wahyuadin <wahyuadin@netsiankomputer.com>
2026-05-07 00:15:14 +0700

    add seeder RKAP

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,9 +13,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            BranchOfficeSeeder::class,
-            UserSeeder::class,
-            KategoriSeeder::class,
+            // BranchOfficeSeeder::class,
+            // UserSeeder::class,
+            // KategoriSeeder::class,
+            SLASeeder::class,
         ]);
     }
 }
